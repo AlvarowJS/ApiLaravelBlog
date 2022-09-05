6b96ad33a7ad294f92cd2c3769a5533dc1f2a94c
AlvaroJS <alvaruwiiu@hotmail.com>
2022-09-04 21:15:44 -0500

    primer commit

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
+
+Route::apiResource('v1/posts', PostV1::class)->only(['destroy','index','show']);
+
+Route::apiResource('v2/posts', PostV2::class)->only(['index','show']);
